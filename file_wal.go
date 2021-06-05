8a98b49929a0116ae596d84c2650fd389210a879
nakabonne <ryo@nakao.dev>
2021-06-05 16:23:59 +0900

    Implement file WAL

--- file_wal.go
+++ file_wal.go
@@ -1,9 +1,16 @@
 package tstorage
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"os"
 	"sync"
+)
+
+var (
+	// Magic sequence to check for valid data.
+	walMagic = uint32(0x11141993)
 )
 
 type fileWAL struct {
@@ -26,9 +33,42 @@ func newFileWal(filename string) (wal, error) {
 
 // append appends the given entry to the end of a file via the file descriptor it has.
 func (w fileWAL) append(entry walEntry) error {
+	// FIXME: Implement appending to wal correctly.
+
 	if w.f == nil {
 		return fmt.Errorf("no file descriptor")
 	}
-	// TODO: Implement appending to wal.
+
+	// Buffer writes until the end.
+	buf := &bytes.Buffer{}
+	var err error
+
+	// Write the operation type
+	if err = buf.WriteByte(byte(entry.operation)); err != nil {
+		return err
+	}
+
+	for _, row := range entry.rows {
+		// Write metric name length
+		name := marshalMetricName(row.Metric, row.Labels)
+		if _, err := buf.WriteString(name); err != nil {
+			return err
+		}
+		if err := binary.Write(buf, binary.LittleEndian, row.DataPoint.Timestamp); err != nil {
+			return err
+		}
+		if err := binary.Write(buf, binary.LittleEndian, row.DataPoint.Value); err != nil {
+			return err
+		}
+	}
+
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	// Flush to the file.
+	if _, err := w.f.Write(buf.Bytes()); err != nil {
+		return err
+	}
+
 	return nil
 }
